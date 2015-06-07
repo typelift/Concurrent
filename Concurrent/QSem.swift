ab4ad5d55f8ebac8e61b74aba299e898bcbf380d
Robert Widmann <devteam.codafi@gmail.com>
2015-06-07 00:26:13 -0600

    Remove bridging header

--- Concurrent/QSem.swift
+++ Concurrent/QSem.swift
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 TypeLift. All rights reserved.
 //
 
-import Swiftz
-
 /// QSem is a simple quanitity semaphore (read: counting semaphore) that aquires and releases
 /// resources in increments of 1.  The semaphore keeps track of blocked threads with MVar<()>'s.
 /// When a thread becomes unblocked, the semaphore simply fills the MVar with a ().  Threads can
