b82a9caa44688c10d98152e5e5564b925ef6886d
Robert Widmann <devteam.codafi@gmail.com>
2015-07-07 22:32:18 -0600

    Remove Dependence on Swiftz

--- Concurrent/Future.swift
+++ Concurrent/Future.swift
@@ -6,27 +6,25 @@
 //  Copyright (c) 2014 TypeLift. All rights reserved.
 //
 
-import Swiftz
-
 public struct Future<A> {
 	let threadID : MVar<Optional<ThreadID>>
-	let cResult : IVar<Result<A>>
-	let finalizers : MVar<[Result<A> -> ()]>
+	let cOptional : IVar<Optional<A>>
+	let finalizers : MVar<[Optional<A> -> ()]>
 
-	private init(_ threadID : MVar<Optional<ThreadID>>, _ cResult : IVar<Result<A>>, _ finalizers : MVar<[Result<A> -> ()]>) {
+	private init(_ threadID : MVar<Optional<ThreadID>>, _ cOptional : IVar<Optional<A>>, _ finalizers : MVar<[Optional<A> -> ()]>) {
 		self.threadID = threadID
-		self.cResult = cResult
+		self.cOptional = cOptional
 		self.finalizers = finalizers
 	}
 	
-	public func read() -> Result<A> {
-		return self.cResult.read()
+	public func read() -> Optional<A> {
+		return self.cOptional.read()
 	}
 	
-	public func then(finalize : Result<A> -> ()) -> Future<A> {
+	public func then(finalize : Optional<A> -> ()) -> Future<A> {
         do {
-            let ma : Optional<Result<A>> = try self.finalizers.modify { sTodo in
-                let res = self.cResult.tryRead()
+            let ma : Optional<Optional<A>> = try self.finalizers.modify { sTodo in
+                let res = self.cOptional.tryRead()
                 if res == nil {
                     return (sTodo + [finalize], res)
                 }
@@ -37,7 +35,7 @@ public struct Future<A> {
             case .None:
                 return self
             case .Some(let val):
-                self.runFinalizerWithResult(val)(finalize)
+                self.runFinalizerWithOptional(val)(finalize)
                 return self
             }
         } catch _ {
@@ -45,64 +43,49 @@ public struct Future<A> {
         }
 	}
 	
-	private func complete(r : Result<A>) -> Result<A> {
-		return self.cResult.tryPut(r) ? r : self.cResult.read()
+	private func complete(r : Optional<A>) -> Optional<A> {
+		return self.cOptional.tryPut(r) ? r : self.cOptional.read()
 	}
 	
-	private func runFinalizerWithResult<A>(val : Result<A>) -> (Result<A> -> ()) -> ThreadID {
+	private func runFinalizerWithOptional<A>(val : Optional<A>) -> (Optional<A> -> ()) -> ThreadID {
 		return { todo in forkIO(todo(val)) }
 	}
 }
 
-public func forkFutures<A>(ios : [() -> A]) -> Chan<Result<A>> {
-	let c : Chan<Result<A>> = Chan()
+public func forkFutures<A>(ios : [() -> A]) -> Chan<Optional<A>> {
+	let c : Chan<Optional<A>> = Chan()
 	_ = ios.map({ forkFuture($0) }).map({ f in f.then({ c.write($0) }) })
 	return c
 }
 
 public func forkFuture<A>(io : () throws -> A) -> Future<A> {
-	let msTid : MVar<Optional<ThreadID>> = MVar()
-	let result : IVar<Result<A>> = IVar()
-	let msTodo : MVar<[Result<A> -> ()]> = MVar(initial: [])
+	let msTid = MVar<ThreadID?>()
+	let Optional : IVar<A?> = IVar()
+	let msTodo : MVar<[A? -> ()]> = MVar(initial: [])
 
-	let p = Future(msTid, result, msTodo)
-	let act : () throws -> () = {
+	let p = Future(msTid, Optional, msTodo)
+	let act : () -> () = {
 		p.threadID.put(.Some(myTheadID()))
-		
-		let val : Result<A> = { res in
-			return p.complete(res)
-        }({
-        do {
-            return Result.Value(try io())
-        } catch let e {
-            return Result.Error(e as NSError)
-        }
-        }())
-		
-		switch val {
-			case .Error(let err):
-				throw err
-			case .Value(_):
-				return ()
-		}
+		_ = p.complete({
+			do {
+				return try io()
+			} catch _ {
+				return nil
+			}
+		}())
 	}
 
 	let process : dispatch_block_t = {
-		let paranoid = Result<A>.error(NSError(domain: "", code: 0, userInfo: nil))
-		
-		p.threadID.modify_(const(.None))
+		let paranoid : A? = nil
+		p.threadID.modify_({ _ in .None })
 		let val = p.complete(paranoid)
 		let sTodo = p.finalizers.swap([])
-		let _ = sTodo.map(p.runFinalizerWithResult(val))
+		let _ = sTodo.map(p.runFinalizerWithOptional(val))
 	}
 
     _ = forkIO {
-        do {
-            try act()
-            process()
-        } catch _ {
-            process()
-        }
+		act()
+		process()
     }
 	return p
 }
